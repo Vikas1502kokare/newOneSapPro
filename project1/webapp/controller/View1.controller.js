d55240483d46a1929a6be07e073904e1ea208457
vikas kokare <vikasskokare1502@gmail.com>
2025-05-28 14:16:55 +0530

    New Fragment Add

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,13 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
+], (Controller, Fragment) => {
     "use strict";
 
     return Controller.extend("com.project1.project1.controller.View1", {
         onInit() {
         },
-        onclick:function(){
-            
+        oclick: function () {
+            if (!this.pDialog) {
+                this.pDialog = Fragment.load({
+                    name: "com.project1.project1.fragment.Open",
+                    controller: this
+                }).then((pDialog) => {
+                    this.getView().addDependent(pDialog)
+                    this.pDialog = pDialog
+                    this.pDialog.open();
+                })
+            } else {
+                this.pDialog.open();
+            }
         },
+        onCloseDialog: function () {
+            this.pDialog.close();
+        }
     });
 });
